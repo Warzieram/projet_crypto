2833baa8a6cce123c70b5f1cacb8dff564ec8fe0
Warzieram <luko974@gmail.com>
2024-04-26 19:28:47 +0200

    projet fini

---
+++ README.md
@@ -0,0 +1,10 @@
+# DM de Cryptographie RAMASSAMY Luc
+
+Pour executer le projet, se positionner à sa racine puis:
+```bash
+python main.py
+```
+
+Le code répondant aux questions pratiques se trouve dans les fonctions au nom correspondantes.
+Les réponess aux questions théoriques se trouvents dans le fichier LucRAMASSAMY.pdf
+
--- main.py
+++ main.py
@@ -22,10 +22,15 @@ class LFSR:
     
     def setState(self, state):
         self.state = state
+
+    def getState(self):
+        return self.state
     
         
 
     def generate_bit(self):
+        """Fonction qui simule un coup d'horloge du LFSR en retournant le bit sorti et mettant à jour l'état en fonction des coefficients de rétroaction"""
+
         output_bit = self.state & 1
         retroaction_bit = 0
         for i in range(self.length):
@@ -58,7 +63,12 @@ def test_lfsr(seed, coeffs):
 
 
 class CSS():
+    """
+    Classe simulant le fonctionnement d'un CSS
+    """
     def __init__(self) -> None:
+        #initialisation des LFSR et de leurs coefficients de rétroaction
+
         coeffs_LFSR_17 = [0] * 17
 
         coeffs_LFSR_17[0] = 1 
@@ -74,9 +84,10 @@ class CSS():
         self.lfsr17 = LFSR(1, coeffs_LFSR_17)
         self.lfsr25 = LFSR(1, coeffs_LFSR_25)
 
-        self.k = 1
+        self.z = None #initialisationd de la cle de chiffrement/dechiffrement
 
     def generate_key(self, s, n_bytes):
+        """Fonction qui génère une clé de chiffrement/dechiffrement z de longueur n_bytes à partir de la clé secrete s"""
         
         seed_lfsr_17 = (s >> 24) & 0xFFFF | 1 << 16
         seed_lfsr_25 =  s & 0xFFFFFF | 1 << 24
@@ -86,12 +97,12 @@ class CSS():
         self.lfsr17.setState(seed_lfsr_17)
         self.lfsr25.setState(seed_lfsr_25)
 
-        x = 0
-        y = 0
-        c = 0
-        k = 0
+        x = 0 #les octets qui sortent du LFSR de longueur 17
+        y = 0 #les octets qui sortent du LFSR de longueur 25
+        c = 0 #le bit de carry
+        k = 0 #la cle de chiffrement/dechiffrement
 
-
+        #on récupère les n_bytes premiers octets produits par chaque LFSR
         for i in range(n_bytes):
             x = 0
             y = 0
@@ -100,7 +111,7 @@ class CSS():
                 y = y | (self.lfsr25.generate_bit() << j) 
 
 
-            
+            #on ajoute chaque octet au à la cle de chiffrement/dechiffrement selon la formule z = x + y + c % 256
             k = k | ((x + y + c) % 256 << (n_bytes-1-i)*8)
 
             if (x+y) > 255:
@@ -108,10 +119,13 @@ class CSS():
             else:
                 c = 0
 
-        self.k = k
+        self.z = k
         return k
     
     def encrypt_message(self, s, message):
+        """Fonction qui permet de chiffrer un message avec un cle secrete"""
+
+        #ici on récupere la longueur l en octet du message pour créer une clé créée à partir des l premiers octets du CSS
         length = 0
         val = message
         if(val == 0):
@@ -122,25 +136,58 @@ class CSS():
                 val = val >> 8
 
         self.generate_key(s, length)
-        return self.k ^ message
+
+        #puis on retourne la clé XOR message
+        return self.z ^ message
+
+    def decrypt_message(self, message):
+        """Fonction qui décrypte un message précedemment encrypté par le css"""
+
+        return self.z ^ message
 
 
+def Question1():
+    print("--------------------------------------------------------------------------------------")
+    print("QUESTION 1")
+
+
+    coeffs_LFSR_17 = [0] * 17
+
+    coeffs_LFSR_17[0] = 1 
+    coeffs_LFSR_17[14] = 1
+
+    seed_17 = random.getrandbits(16) | 1 << 16 #génération d'un état initial valide aléatoire
+
+    is_full_cycle = test_lfsr(seed_17, coeffs_LFSR_17)
+
+    print(f"Etat initial: {hex(seed_17)}")
+    print("Le LSFR passe par toutes les valeurs non-nulles: ", is_full_cycle)
+    print('---------------------------------------------------------------------------------------')
 
 def Question4():
+    print("--------------------------------------------------------------------------------------")
+    print("QUESTION 4")
     message = 0xffffffffff
 
     css = CSS()
     s = 0
 
     message_chiffre = css.encrypt_message(s, message)
+    message_dechiffre = css.decrypt_message(message_chiffre)
+
+    print(f"Le message de base est: {hex(message)}")
+    print(f"Une fois chiffré le message est: {hex(message_chiffre)}")
+    print(f"Et une fois déchifré le message est {hex(message_dechiffre)}")
+
+    print("--------------------------------------------------------------------------------------")
 
 
-    print(hex(message_chiffre))
+def Question6():
 
-#Question4()
+    print("--------------------------------------------------------------------------------------")
+    print("QUESTION 6")
 
-
-def Question5():
+    #initilaisation des coefficients de rétroaction pour les LFSR
 
     coeffs_LFSR_17 = [0] * 17
 
@@ -154,73 +201,84 @@ def Question5():
     coeffs_LFSR_25[4] = 1
     coeffs_LFSR_25[12] = 1
 
-    # Try every possible state of the 17-bit LFSR
+    secret_key = random.getrandbits(40) #génere un nombre aléatoire de 40 bits qui va nous servie de clé secrete
+    seed17 = 1 << 16 | (secret_key >> 24) & 0xFFFF
+    seed25 = 1 << 24 | (secret_key) & 0xFFFFFFFF
 
-    premier_etat = 2 ** 16
-    dernier_etat = 2 ** 17
-
-    secret_key = random.getrandbits(40)
     css = CSS()
-    z = [css.generate_key(secret_key, 6) >>(8*i) & 0xFF for i in range (6)] #générer z1 à z6
+    _z = css.generate_key(secret_key, 6)# génère les 6 premiers octets de z
+    z = [_z >>(8*i) & 0xFF for i in range (6)][::-1] #liste qui divise z en 6
+    
 
-    found_s = 0
-
-
-    for initial_state_17 in range(premier_etat, dernier_etat): #on teste tous les états possibles du premier LFSR
-
-        lfsr17 = LFSR(initial_state_17, coeffs_LFSR_17)
-        x = [] #liste de x1 à x6
-
-        for i in range(6):
-            o = 0
-            for j in range(8):
-                o = o | lfsr17.generate_bit() << j  #on génère x1 à x6
-            x.append(o)
-        
-        y = [] # on va calculer y1 à y6 à partir de x et z
+    for _etat in range(0, 2**16): #on boucle sur tous les états possibles du LFSR de longueur 17
+   
         c = 0
-        for i in range(6):
-            if(z[i] - x[i] > 0):
-                c = 0
-                y.append(z[i] - x[i] - c)
+        etat_init = 1 << 16 | _etat
+        lfsr17 = LFSR(etat_init, coeffs_LFSR_17)
+
+        #on récupère x1 et x2 qui sont les deux derniers octets de l'etat initial
+        x1 = lfsr17.getState() % 256
+        x2 = (lfsr17.getState() >> 8) % 256 
+        
+        #on génère x3
+        for _ in range(16):
+            lfsr17.generate_bit()
+        
+        x3 = lfsr17.getState() % 256
+
+        
+        #à partir de là on peut calculer y1, y2 et y3
+        y1 = (z[0] - x1 - c)%256
+        c = 1 if (x1 + y1 > 255) else 0
+
+        y2 = (z[1] - x2 - c)%256
+        c = 1 if (x2 + y2 > 255) else 0
+
+        y3 = (z[2] - x3 - c)%256
+        c = 1 if (x3 + y3 > 255) else 0
+
+        #on peut alors calculer z3, z5, et z6 et les comparer avec les z originaux pour vérifier la cohérence des résultats
+        etat_initial_25 = 1 << 24 | y3 << 16 | y2 << 8 | y1   
+        lfsr25 = LFSR(etat_initial_25, coeffs_LFSR_25)
+
+        for _ in range(24):
+            lfsr25.generate_bit()
+        for _ in range(8):
+            lfsr17.generate_bit()
+        
+        correspondance = True
+        for i in range(3, 6):
+            x = lfsr17.getState() % 256
+            y = lfsr25.getState() % 256
+
+            if z[i] != (x + y + c) % 256:
+                correspondance = False
+                break
             else:
-                c = 1
-                y.append(z[i] - x[i] - c + 256)
-        
-        test_seed = 1 << 24 | y[0] << 16 | y[1] << 8 | y[2]
-
-        test_bytes = []
-
-        test_lfsr25 = LFSR(test_seed, coeffs_LFSR_25)
-
-        for i in range(6):
-            g= 0
-            for j in range(8):
-                g = g | test_lfsr25.generate_bit() << j
-            test_bytes.append(g)
-        
-        correspondance = (test_bytes == y)
-
-        if correspondance:
-            found_s = x[0][0] << 32 | x[1][0] << 24 | y[0] << 16 | y[1] << 8 | y[0]
-            break
-            
+                for _ in range(8):
+                    lfsr17.generate_bit()
+                    lfsr25.generate_bit()
+                c = 1 if x + y > 255 else 0
+                continue
+        if not correspondance:
+            continue
 
 
-    print(found_s)
-    print(secret_key)
-    print(found_s == secret_key)
-    return found_s == secret_key
+        print("Etat initial du LFSR17 calculé: " + hex((1 << 16) + (x2 << 8) + x1))
+        print("Etat initial du LFSR25 correspondant: " + hex((1 << 24) + (y3 << 16) + (y2 << 8) + y1))
+        print("Etat initial du LFSR17 original: " + hex(seed17))
+        print("Etat initial du LFSR25 original:" + hex(seed25))
+        break
+    
+    print("--------------------------------------------------------------------------------------")
+    
+    return
 
-Question5()
-
-"""
-
-#Partie de test à décommenter pour executer la fonction de test sur un LFSR de taille 17
-# Executer la fonction de test
-
-is_full_cycle = test_lfsr(seed_17, coeffs_LFSR_17)
-print("Le LSFR pass par toutes les valeurs non-nulles: ", is_full_cycle)
+Question1()
+Question4()
+Question6()
 
 
-"""
+
+
+
